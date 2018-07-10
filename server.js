cdfc3cb780169bdd29c1eb9305c73b1aee498d1b
Harley Jarrett <harley.jarrett@griffithuni.edu.au>
2018-07-10 07:42:44 +0000

    game begining

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-var http = require('http');
-var path = require('path');
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
+//Using
+const http = require('http');
+const path = require('path');
+const async = require('async');
+const socketio = require('socket.io');
+const express = require('express');
 const bodyParser = require('body-parser')
-var app = express();
-var server = http.createServer(app);
-var io = socketio.listen(server);
+const app = express();
+const server = http.createServer(app);
+const io = socketio.listen(server);
+
+//Controllers
+// const routes = require('./controllers/routes')
 
 
-const routes = require('./controllers/routes')
+//Models
 
 
+//Views
 
+
+//Route configure
 app.use(express.static(path.resolve(__dirname, 'views')));
 app.use(express.static(__dirname+'/node_modules'));
 app.use( bodyParser.json() );
@@ -24,12 +31,12 @@ var messages = [];
 var sockets = [];
 
 
+//Routes
+// app.get('/test', routes.test_route)
 
-app.get('/test', routes.test_route)
+// app.get('/get_data', routes.get_data)
 
-app.get('/get_data', routes.get_data)
-
-app.post('/login', routes.send_login)
+// app.post('/login', routes.send_login)
 
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
